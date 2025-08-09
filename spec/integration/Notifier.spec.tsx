b45a9e4cff53bca3ff6fabf343632e3e2d082857
Alexey Ushakov <develengie@gmail.com>
2025-08-09 18:18:13 +0300

    test(notifier): wrote integration tests

--- spec/integration/Notifier.spec.tsx
+++ spec/integration/Notifier.spec.tsx
@@ -1,7 +1,69 @@
-import { render, screen } from "@testing-library/react";
-import { Notifier } from "src/components/Notifier";
+import { render } from "@testing-library/react";
+import ue from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
+import { App } from "src/App";
+import { JestStoreProvider } from "../utils/JestStoreProvider";
 
-describe('Оповещение при вополнении задачи', () => {
-    it.todo('появляется и содержит заголовок задачи');
-    it.todo('одновременно может отображаться только одно');
+const userEvent = ue.setup({
+    advanceTimers: jest.advanceTimersByTime,
+});
+
+jest.useFakeTimers();
+
+describe("Оповещение при выполнении задачи", () => {
+    it("появляется и содержит заголовок задачи", async () => {
+        const { getByRole, container, findByText } = render(<App />, {
+            wrapper: JestStoreProvider,
+        });
+
+        const input = getByRole("textbox");
+        const addButton = container.querySelector(
+            '[data-alt="добавить задачу"]'
+        );
+
+        await userEvent.type(input, "Задача 1");
+        await userEvent.click(addButton!);
+
+        const task = getByRole("checkbox");
+
+        await userEvent.click(task);
+
+        const notifier = await findByText(/Задача "Задача 1" завершена/i);
+
+        expect(notifier).toBeInTheDocument();
+
+        act(() => jest.advanceTimersByTime(2000));
+
+        expect(notifier).not.toBeInTheDocument();
+    });
+
+    it("одновременно может отображаться только одно", async () => {
+        const { getByRole, container, getAllByRole, findByText, getAllByText } =
+            render(<App />, {
+                wrapper: JestStoreProvider,
+            });
+
+        const input = getByRole("textbox");
+        const addButton = container.querySelector(
+            '[data-alt="добавить задачу"]'
+        );
+
+        await userEvent.type(input, "Задача 2");
+        await userEvent.click(addButton!);
+
+        const allTasks = getAllByRole("checkbox");
+
+        await userEvent.click(allTasks[0]);
+        await userEvent.click(allTasks[0]);
+
+        const notifier = await findByText(/Задача "Задача 1" завершена/i);
+
+        expect(notifier).toBeInTheDocument();
+
+        await userEvent.click(allTasks[1]);
+
+        const allNotifiers = getAllByText(/Задача "/i);
+
+        expect(allNotifiers).toHaveLength(1);
+    });
 });
